6c0bfc93ec91a58c3cde547aa6f760389fea0a3f
Mosseelight <doctorphillip233@gmail.com>
2024-01-05 23:41:37 -0500

    Networking packet stuff

--- Src/Networking/NetCommon.cs
+++ Src/Networking/NetCommon.cs
@@ -1,13 +1,17 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Numerics;
 using System.Reflection.Emit;
 using JoltPhysicsSharp;
+using Riptide;
 using Silk.NET.Core;
 using Silk.NET.Input;
 
+
 //engine stuff
 using static SpatialEngine.Globals;
+using static SpatialEngine.Networking.IPackets;
 using static SpatialEngine.Rendering.MeshUtils;
 
 namespace SpatialEngine.Networking
@@ -19,271 +23,134 @@ namespace SpatialEngine.Networking
         Pong,
         Connect,
         ConnectReturn,
-        SpatialObjectPacket,
-        SpawnSpatialObject,
+        SpatialObject,
+        SpawnSpatialObject
+    }
+
+    public interface IPackets
+    {
+        public ushort GetPacketType();
+        public byte[] GetBytes();
+
+        public record PingPacket() : IPackets
+        {
+            public ushort GetPacketType() => (ushort)PacketType.Ping;
+            public byte[] GetBytes()
+            {
+                return BitConverter.GetBytes((ushort)PacketType.Ping);
+            }
+        }
+        public record PongPacket()
+        {
+            public ushort GetPacketType() => (ushort)PacketType.Pong;
+            public byte[] GetBytes()
+            {
+                return BitConverter.GetBytes((ushort)PacketType.Pong);
+            }
+        }
+        public record ConnectPacket()
+        {
+            public ushort GetPacketType() => (ushort)PacketType.Connect;
+            public byte[] GetBytes()
+            {
+                return BitConverter.GetBytes((ushort)PacketType.Connect);
+            }
+        }
+        public record ConnectReturnPacket()
+        {
+            public ushort GetPacketType() => (ushort)PacketType.ConnectReturn;
+            public byte[] GetBytes()
+            {
+                List<byte> bytes = new List<byte>();
+                bytes.AddRange(BitConverter.GetBytes((ushort)PacketType.ConnectReturn));
+                for (int i = 0; i < EngVer.Length; i++)
+                {
+                    bytes.Add(Convert.ToByte(EngVer[i]));
+                }
+                return bytes.ToArray();
+            }
+        }
+        public record SpatialObjectPacket(int id, Vector3 position, Quaternion rotation)
+        {
+            public ushort GetPacketType() => (ushort)PacketType.SpatialObject;
+            public byte[] GetBytes()
+            {
+                List<byte> bytes = new List<byte>();
+                bytes.AddRange(BitConverter.GetBytes((ushort)PacketType.SpatialObject));
+                bytes.AddRange(BitConverter.GetBytes(id));
+                bytes.AddRange(BitConverter.GetBytes(position.X));
+                bytes.AddRange(BitConverter.GetBytes(position.Y));
+                bytes.AddRange(BitConverter.GetBytes(position.Z));
+                bytes.AddRange(BitConverter.GetBytes(rotation.X));
+                bytes.AddRange(BitConverter.GetBytes(rotation.Y));
+                bytes.AddRange(BitConverter.GetBytes(rotation.Z));
+                bytes.AddRange(BitConverter.GetBytes(rotation.W));
+                return bytes.ToArray();
+            }
+        }
+        public record SpawnSpatialObjectPacket(Vector3 position, Quaternion rotation, string modelLocation, ushort motionType, ushort ObjectLayer, ushort Activation)
+        {
+            public ushort GetPacketType() => (ushort)PacketType.SpawnSpatialObject;
+            public byte[] GetBytes()
+            {
+                List<byte> bytes = new List<byte>();
+                bytes.AddRange(BitConverter.GetBytes((ushort)PacketType.SpawnSpatialObject));
+                bytes.AddRange(BitConverter.GetBytes(position.X));
+                bytes.AddRange(BitConverter.GetBytes(position.Y));
+                bytes.AddRange(BitConverter.GetBytes(position.Z));
+                bytes.AddRange(BitConverter.GetBytes(rotation.X));
+                bytes.AddRange(BitConverter.GetBytes(rotation.Y));
+                bytes.AddRange(BitConverter.GetBytes(rotation.Z));
+                bytes.AddRange(BitConverter.GetBytes(rotation.W));
+                for (int i = 0; i < modelLocation.Length; i++)
+                {
+                    bytes.Add(Convert.ToByte(modelLocation[i]));
+                }
+                bytes.AddRange(BitConverter.GetBytes(motionType));
+                bytes.AddRange(BitConverter.GetBytes(ObjectLayer));
+                bytes.AddRange(BitConverter.GetBytes(Activation));
+                return bytes.ToArray();
+            }
+        }
     }
     
-    public abstract class Packet
+    public class Packet
     {
         // may be better to have the handle packet here and it returns a record of the packet type i need
         // handles whatever data and returns the packet with the data
-    
-        public abstract byte[] ConvertToByte();
-        public abstract void ByteToPacket(byte[] data);
-    }
-    
-    public abstract class ServerPacket
-    {
-    
-        public abstract byte[] ConvertToByte();
-        public abstract void ByteToPacket(byte[] data);
-    }
-    
-    public abstract class ClientPacket
-    {
-    
-        public abstract byte[] ConvertToByte();
-        public abstract void ByteToPacket(byte[] data);
-    }
-    
-    
-    //Connection packets
-    
-    public class ConnectPacket : ClientPacket
-    {
-        public ConnectPacket() 
+
+        public Packet()
         {
             
         }
-    
-        public override byte[] ConvertToByte() 
+
+        public byte[] ConvertToByte<T>(T packet) where T : IPackets
         {
-            MemoryStream stream = new MemoryStream();
-            BinaryWriter writer = new BinaryWriter(stream);
-            writer.Write((ushort)PacketType.Connect);
-            return stream.ToArray();
+            switch (packet.GetPacketType())
+            {
+                case (ushort)PacketType.Ping:
+                    return packet.GetBytes();
+                case (ushort)PacketType.Pong:
+                    return packet.GetBytes();
+                case (ushort)PacketType.Connect:
+                    return packet.GetBytes();
+                case (ushort)PacketType.ConnectReturn:
+                    return packet.GetBytes();
+                case (ushort)PacketType.SpatialObject:
+                    return packet.GetBytes();
+                case (ushort)PacketType.SpawnSpatialObject:
+                    return packet.GetBytes();
+            }
+
+            // if reach here input packet is bad or not implement
+            throw new Exception("Packet not found");
         }
-    
-        public override void ByteToPacket(byte[] data)
+        public void ByteToPacket(byte[] data)
         {
-            //does nothing
+
         }
     }
     
-    public class ConnectReturnPacket : ServerPacket
-    {
-        public ConnectReturnPacket()
-        {
-    
-        }
-    
-        public override byte[] ConvertToByte()
-        {
-            MemoryStream stream = new MemoryStream();
-            BinaryWriter writer = new BinaryWriter(stream);
-            writer.Write(EngVer);
-            throw new NotImplementedException();
-    
-        }
-    
-        public override void ByteToPacket(byte[] data)
-        {
-            //does nothing
-        }
-    }
-
-    //Packet for getting time between sent packets
-    //checks just based on the enum of packets
-
-    public class PingPacket : ClientPacket
-    {
-        public PingPacket()
-        {
-
-        }
-
-        public override byte[] ConvertToByte()
-        {
-            MemoryStream stream = new MemoryStream();
-            BinaryWriter writer = new BinaryWriter(stream);
-            writer.Write((ushort)PacketType.Ping);
-            return stream.ToArray();
-        }
-
-        public override void ByteToPacket(byte[] data)
-        {
-            //does nothing
-        }
-    }
-
-    public class PongPacket : ServerPacket
-    {
-        public PongPacket()
-        {
-
-        }
-
-        public override byte[] ConvertToByte()
-        {
-            MemoryStream stream = new MemoryStream();
-            BinaryWriter writer = new BinaryWriter(stream);
-            writer.Write((ushort)PacketType.Pong);
-            return stream.ToArray();
-        }
-
-        public override void ByteToPacket(byte[] data)
-        {
-            //does nothing
-        }
-    }
-    
-
-    //sync packets
-    
-    //will need more infomration then this like info about the rigidbody state and whatever
-    public class SpatialObjectPacket : ServerPacket
-    {
-        public int id;
-        public Vector3 Position;
-        public Quaternion Rotation;
-    
-        public SpatialObjectPacket()
-        {
-    
-        }
-    
-        public SpatialObjectPacket(int id, Vector3 position, Quaternion rotation)
-        {
-            this.id = id;
-            this.Position = position;
-            this.Rotation = rotation;
-        }
-    
-        public override byte[] ConvertToByte()
-        {
-            MemoryStream stream = new MemoryStream();
-            BinaryWriter writer = new BinaryWriter(stream);
-            //type of packet
-            writer.Write((ushort)PacketType.SpatialObjectPacket);
-            //id
-            writer.Write(id);
-            //position
-            writer.Write(Position.X);
-            writer.Write(Position.Y);
-            writer.Write(Position.Z);
-            //rotation
-            writer.Write(Rotation.X);
-            writer.Write(Rotation.Y);
-            writer.Write(Rotation.Z);
-            writer.Write(Rotation.W);
-            stream.Close();
-            writer.Close();
-    
-            return stream.ToArray();
-        }
-    
-        public override void ByteToPacket(byte[] data)
-        {
-            MemoryStream stream = new MemoryStream(data);
-            BinaryReader reader = new BinaryReader(stream);
-            ushort type = reader.ReadUInt16();
-            id = reader.ReadInt32();
-            //position
-            float posX = reader.ReadSingle();
-            float posY = reader.ReadSingle();
-            float posZ = reader.ReadSingle();
-            Position = new Vector3(posX, posY, posZ);
-            //rotation
-            float rotX = reader.ReadSingle();
-            float rotY = reader.ReadSingle();
-            float rotZ = reader.ReadSingle();
-            float rotW = reader.ReadSingle();
-            Rotation = new Quaternion(rotX, rotY, rotZ, rotW);
-            stream.Close();
-            reader.Close();
-        }
-    }
-    
-    public class SpawnSpatialObjectPacket : ClientPacket
-    {
-        public Vector3 Position;
-        public Quaternion Rotation;
-        public string ModelLocation;
-        public int MotionType;
-        public int ObjectLayer;
-        public int Activation;
-    
-        public SpawnSpatialObjectPacket()
-        {
-    
-        }
-    
-        public SpawnSpatialObjectPacket(int id, Vector3 position, Quaternion rotation, string modelLocation, MotionType motionType, ObjectLayer objectLayer, Activation activation)
-        {
-            this.Position = position;
-            this.Rotation = rotation;
-            this.ModelLocation = modelLocation;
-            this.MotionType = (int)motionType;
-            this.ObjectLayer = objectLayer;
-            this.Activation = (int)activation;
-        }
-    
-        public override byte[] ConvertToByte()
-        {
-            MemoryStream stream = new MemoryStream();
-            BinaryWriter writer = new BinaryWriter(stream);
-            //type of packet
-            writer.Write((ushort)PacketType.SpawnSpatialObject);
-            //id
-            //writer.Write(id);
-            //position
-            writer.Write(Position.X);
-            writer.Write(Position.Y);
-            writer.Write(Position.Z);
-            //rotation
-            writer.Write(Rotation.X);
-            writer.Write(Rotation.Y);
-            writer.Write(Rotation.Z);
-            writer.Write(Rotation.W);
-            //modellocation
-            writer.Write(ModelLocation);
-            //motion type
-            writer.Write(MotionType);
-            //object layer
-            writer.Write(ObjectLayer);
-            //activation
-            writer.Write(Activation);
-            stream.Close();
-            writer.Close();
-    
-            return stream.ToArray();
-        }
-    
-        public override void ByteToPacket(byte[] data)
-        {
-            MemoryStream stream = new MemoryStream(data);
-            BinaryReader reader = new BinaryReader(stream);
-            int type = reader.ReadUInt16();
-            //id = reader.ReadInt32();
-            //position
-            float posX = reader.ReadSingle();
-            float posY = reader.ReadSingle();
-            float posZ = reader.ReadSingle();
-            Position = new Vector3(posX, posY, posZ);
-            //rotation
-            float rotX = reader.ReadSingle();
-            float rotY = reader.ReadSingle();
-            float rotZ = reader.ReadSingle();
-            float rotW = reader.ReadSingle();
-            Rotation = new Quaternion(rotX, rotY, rotZ, rotW);
-            ModelLocation = reader.ReadString();
-            MotionType = reader.ReadInt32();
-            ObjectLayer = reader.ReadInt32();
-            Activation = reader.ReadInt32();
-            stream.Close();
-            reader.Close();
-        }
-    }
     
     //what should be done when a packet is recived of any types
     //should work for both client and host/server
