9a9a4df35d0f42ccb1df94213c758c62ee1e7513
Mosseelight <doctorphillip233@gmail.com>
2024-02-29 23:13:41 -0500

    Ui rendering start

---
+++ Src/Rendering/UiRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpatialEngine.Rendering
+{
+
+    public class UiElement
+    {
+        //size for the quad
+        public float Length;
+        public float Height;
+
+        //texture that is displayed
+        public Texture texture;
+    }
+
+    public static class UiRenderer
+    {
+
+    }
+}
