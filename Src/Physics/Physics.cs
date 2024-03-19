3c9b9c8ed5702b509f8639347d4c4ba827a31b75
Mosseelight <doctorphillip233@gmail.com>
2024-03-19 11:25:28 -0400

    Fixed jolt with new update

--- Src/Physics/Physics.cs
+++ Src/Physics/Physics.cs
@@ -24,98 +24,47 @@ namespace SpatialEngine
 	    public static uint NUM_LAYERS = 2;
     }
 
-    public class ObjectLayerPairFilterImpl : ObjectLayerPairFilter
-    {
-        protected override bool ShouldCollide(ObjectLayer inObject1, ObjectLayer inObject2)
-        {
-            switch (inObject1)
-            {
-            case 0: // non moving
-                return inObject2 == Layers.MOVING; // Non moving only collides with moving
-            case 1: // moving
-                return true; // Moving collides with everything
-            default:
-                return false;
-            }
-        }
-    }
-
-    public class ObjectVsBroadPhaseLayerFilterImpl : ObjectVsBroadPhaseLayerFilter
-    {
-        protected override bool ShouldCollide(ObjectLayer inLayer1, BroadPhaseLayer inLayer2)
-        {
-            switch (inLayer1)
-            {
-            case 0: // non moving
-                return inLayer2 == BroadPhaseLayers.MOVING;
-            case 1: // moving
-                return true;
-            default:
-                return false;
-            }
-        }
-    }
-
-    public class BPLayerInterfaceImpl : BroadPhaseLayerInterface
-    {
-
-        BroadPhaseLayer[] mObjectToBroadPhase = new BroadPhaseLayer[Layers.NUM_LAYERS];
-
-        public BPLayerInterfaceImpl()
-        {
-            // Create a mapping table from object to broad phase layer
-            mObjectToBroadPhase[Layers.NON_MOVING] = BroadPhaseLayers.NON_MOVING;
-            mObjectToBroadPhase[Layers.MOVING] = BroadPhaseLayers.MOVING;
-        }
-
-        protected override int GetNumBroadPhaseLayers()
-        {
-            return (int)BroadPhaseLayers.NUM_LAYERS;
-        }
-
-        protected override BroadPhaseLayer GetBroadPhaseLayer(ObjectLayer inLayer)
-        {
-            return mObjectToBroadPhase[inLayer];
-        }
-
-        protected override string GetBroadPhaseLayerName(BroadPhaseLayer inLayer)
-        {
-            switch (inLayer)
-            {
-            case 0: // non moving	
-                return "NON_MOVING";
-            case 1: // moving		
-                return "MOVING";
-            default:													
-                return "INVALID";
-            }
-        }
-    }
-
     public unsafe class Physics
     {
-        static TempAllocator tempAllocator;
-        static JobSystemThreadPool jobSystem;
+        static readonly int maxObjects = 65536;
+        static readonly int maxObjectMutex = 0;
+        static readonly int maxObjectPairs = 65536;
+        static readonly int maxContactConstraints = 10240;
 
-        static readonly uint maxObjects = 65536;
-        static readonly uint maxObjectMutex = 0;
-        static readonly uint maxObjectPairs = 65536;
-        static readonly uint maxContactConstraints = 10240;
-
-        BPLayerInterfaceImpl layerInterface;
-        ObjectVsBroadPhaseLayerFilterImpl objectVsBroadPhaseLayer;
-        ObjectLayerPairFilterImpl objectLayerPair;
+        ObjectLayerPairFilter objectLayerPairFilter;
+        BroadPhaseLayerInterface broadPhaseLayerInterface;
+        ObjectVsBroadPhaseLayerFilter objectVsBroadPhaseLayerFilter;
+        
 
         public void InitPhysics()
         {
-            Foundation.Init(false);
-            tempAllocator = new TempAllocator(10 * 1024 * 1024);
-            jobSystem = new JobSystemThreadPool(Foundation.MaxPhysicsJobs, Foundation.MaxPhysicsBarriers, Process.GetCurrentProcess().Threads.Count / 2);
-            physicsSystem = new PhysicsSystem();
-            layerInterface = new BPLayerInterfaceImpl();
-            objectVsBroadPhaseLayer = new ObjectVsBroadPhaseLayerFilterImpl();
-            objectLayerPair = new ObjectLayerPairFilterImpl();
-            physicsSystem.Init(maxObjects, maxObjectMutex, maxObjectPairs, maxContactConstraints, layerInterface, objectVsBroadPhaseLayer, objectLayerPair);
+            Foundation.Init(0u, false);
+
+            ObjectLayerPairFilterTable objectLayerPairFilterTable = new(2);
+            objectLayerPairFilterTable.EnableCollision(Layers.NON_MOVING, Layers.MOVING);
+            objectLayerPairFilterTable.EnableCollision(Layers.MOVING, Layers.MOVING);
+
+            // We use a 1-to-1 mapping between object layers and broadphase layers
+            BroadPhaseLayerInterfaceTable broadPhaseLayerInterfaceTable = new(2, 2);
+            broadPhaseLayerInterfaceTable.MapObjectToBroadPhaseLayer(Layers.NON_MOVING, BroadPhaseLayers.NON_MOVING);
+            broadPhaseLayerInterfaceTable.MapObjectToBroadPhaseLayer(Layers.MOVING, BroadPhaseLayers.MOVING);
+
+            objectLayerPairFilter = objectLayerPairFilterTable;
+            broadPhaseLayerInterface = broadPhaseLayerInterfaceTable;
+            objectVsBroadPhaseLayerFilter = new ObjectVsBroadPhaseLayerFilterTable(broadPhaseLayerInterfaceTable, 2, objectLayerPairFilterTable, 2);
+
+            PhysicsSystemSettings settings = new()
+            {
+                ObjectLayerPairFilter = objectLayerPairFilter,
+                BroadPhaseLayerInterface = broadPhaseLayerInterface,
+                ObjectVsBroadPhaseLayerFilter = objectVsBroadPhaseLayerFilter,
+                MaxBodies = maxObjects,
+                MaxBodyPairs = maxObjectPairs,
+                MaxContactConstraints = maxContactConstraints
+            };
+
+            physicsSystem = new PhysicsSystem(settings);
+
             physicsSystem.OptimizeBroadPhase();
             bodyInterface = physicsSystem.BodyInterface;
         }
@@ -130,7 +79,7 @@ namespace SpatialEngine
                     obj.SO_mesh.rotation = bodyInterface.GetRotation(obj.SO_rigidbody.rbID);
                 }
             }
-            physicsSystem.Update(dt, 3, tempAllocator, jobSystem);
+            physicsSystem.Step(dt, 3);
         }
         
         public void DestroyPhysics(ref Scene scene)
@@ -143,7 +92,6 @@ namespace SpatialEngine
                 }
             }
 
-            tempAllocator.Dispose();
             Foundation.Shutdown();
         }
 
